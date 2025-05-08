8954203c53611a79a206b358f582373f851d32fe
ChickenShot5 <andu_andrei300@yahoo.com>
2025-05-08 15:50:25 +0300

    Use dynamic port for Render deployment

--- Bookbuddy-backend/server.js
+++ Bookbuddy-backend/server.js
@@ -5,7 +5,7 @@ const db = require("./db");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
